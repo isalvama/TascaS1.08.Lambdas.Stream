0d404811f7c7289b0f24af160991490a5b8eb2c6
isalvama <inesalvans@gmail.com>
2026-02-23 13:52:04 +0100

    feat: create Student class with all the required attributes and constructor feat: create ThirdLevelMain class to test the methods and instances of Student class

---
+++ src/main/java/third_level/Student.java
@@ -0,0 +1,15 @@
+package third_level;
+
+public class Student {
+    private String name;
+    private int age;
+    private int studyCourse;
+    private double grade;
+
+    public Student (String name, int age, int studyCourse, double grade){
+        this.name = name;
+        this.age = age;
+        this.studyCourse = studyCourse;
+        this.grade = grade;
+    }
+}
---
+++ src/main/java/third_level/ThirdLevelMain.java
@@ -0,0 +1,18 @@
+package third_level;
+
+/*
+Aquest nivell introdueix l’ús de lambdas i streams aplicats a una classe pròpia, Alumne, per simular situacions reals.
+Filtraràs i transformaràs llistes d’objectes, aplicant múltiples condicions i accions. Això t’ajudarà a veure el valor
+pràctic d’aquestes eines en projectes més realistes, amb dades més estructurades.
+
+Exercici 1
+Omple una llista amb 10 alumnes
+
+Mostra per pantalla el nom i l’edat de cada alumne/a.
+Filtra la llista per tots els alumnes que el seu nom comença per ‘a’. Assigna a aquests alumnes a una altra llista i mostra per pantalla la nova llista (tot amb lambdes).
+Filtra i mostra per pantalla els alumnes que tenen una nota de 5 o superior.
+Filtra i mostra per pantalla els alumnes que tenen una nota de 5 o més, i que no són de PHP.
+Mostra tots els alumnes que fan JAVA i són majors d’edat.
+ */
+public class ThirdLevelMain {
+}
