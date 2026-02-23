cd5d4931578360854ec0bf45b7387a6e7a35a9df
isalvama <inesalvans@gmail.com>
2026-02-23 16:07:58 +0100

    feat: create addStudentWithANameList(), printStudentWith5OrMoreGrade(), printStudentWith5OrMoreGradeNotPHP(), printAdultAndJavaStudents() methods in StudentAnalyzer class, call the new methods in ThirdLevelMain class to test them

--- src/main/java/third_level/StudentAnalyzer.java
+++ src/main/java/third_level/StudentAnalyzer.java
@@ -1,14 +1,49 @@
 package third_level;
+import java.util.ArrayList;
 import java.util.List;
 
+/*
+Filtra la llista per tots els alumnes que el seu nom comença per ‘a’. Assigna a aquests alumnes a una altra llista i mostra per pantalla la nova llista (tot amb lambdes).
+Filtra i mostra per pantalla els alumnes que tenen una nota de 5 o superior.
+Filtra i mostra per pantalla els alumnes que tenen una nota de 5 o més, i que no són de PHP.
+Mostra tots els alumnes que fan JAVA i són majors d’edat.
+ */
+
 public class StudentAnalyzer {
-    List<Student> studentList;
+    private List<Student> studentList;
+    private List<Student> studentWithANameList;
 
     public StudentAnalyzer(List<Student> studentList){
         this.studentList = studentList;
+        this.studentWithANameList = new ArrayList<>();
     }
 
-    public void print(){
-        this.studentList.forEach(sd -> System.out.println("name: " + sd.getName() + "age: " + sd.getAge()));
+    public void printStudentNameAndAge(){
+        this.studentList.forEach(sd -> System.out.println("name: " + sd.getName() + ", age: " + sd.getAge()));
+    }
+
+    public void addStudentWithANameList(){
+        this.studentList.stream().filter(
+                sd -> sd.getName().toLowerCase().charAt(0) == 'a').
+                forEach(sd -> studentWithANameList.add(sd));
+        studentWithANameList.forEach(System.out::println);
+    }
+
+    public void printStudentWith5OrMoreGrade(){
+        this.studentList.stream().filter(
+                        sd -> sd.getGrade() >= 5).
+                forEach(System.out::println);
+    }
+
+    public void printStudentWith5OrMoreGradeNotPHP(){
+        this.studentList.stream().filter(
+                        sd -> sd.getGrade() >= 5).filter(sd -> !sd.getStudyCourse().equalsIgnoreCase("php")).
+                forEach(System.out::println);
+    }
+
+    public void printAdultAndJavaStudents(){
+        this.studentList.stream().filter(
+                        sd -> sd.getGrade() >= 5).filter(sd -> sd.getStudyCourse().equalsIgnoreCase("java")).
+                forEach(System.out::println);
     }
 }
--- src/main/java/third_level/ThirdLevelMain.java
+++ src/main/java/third_level/ThirdLevelMain.java
@@ -3,34 +3,31 @@ package third_level;
 import java.util.Arrays;
 import java.util.List;
 
-/*
-Aquest nivell introdueix l’ús de lambdas i streams aplicats a una classe pròpia, Alumne, per simular situacions reals.
-Filtraràs i transformaràs llistes d’objectes, aplicant múltiples condicions i accions. Això t’ajudarà a veure el valor
-pràctic d’aquestes eines en projectes més realistes, amb dades més estructurades.
-
-Mostra per pantalla el nom i l’edat de cada alumne/a.
-Filtra la llista per tots els alumnes que el seu nom comença per ‘a’. Assigna a aquests alumnes a una altra llista i mostra per pantalla la nova llista (tot amb lambdes).
-Filtra i mostra per pantalla els alumnes que tenen una nota de 5 o superior.
-Filtra i mostra per pantalla els alumnes que tenen una nota de 5 o més, i que no són de PHP.
-Mostra tots els alumnes que fan JAVA i són majors d’edat.
- */
 public class ThirdLevelMain {
     public static void main (String[] args){
         Student student1 = new Student("Joe", 30, "PHP", 6.5);
         Student student2 = new Student("Laura", 35, "Java", 7);
-        Student student3 = new Student("Mark", 27, "Javascript", 8);
-        Student student4 = new Student("Maria", 25, "Javascript", 6.5);
+        Student student3 = new Student("Mark", 27, "Javascript", 4);
+        Student student4 = new Student("Albert", 25, "Javascript", 3);
         Student student5 = new Student("Alex", 30, "PHP", 5);
         Student student6 = new Student("Valerie", 23, "Java", 9);
         Student student7 = new Student("Fernando", 32, "Javascript", 6.5);
-        Student student8 = new Student("Ivan", 31, "Java", 5.5);
-        Student student9 = new Student("Paula", 26, "PHP", 6);
+        Student student8 = new Student("Ivan", 31, "Java", 4.5);
+        Student student9 = new Student("Aura", 26, "PHP", 6);
         Student student10 = new Student("Natalia", 30, "Java", 9);
 
         List<Student> studentList = Arrays.asList(student1, student2, student3, student4, student5, student6, student7, student8, student9, student10);
 
         StudentAnalyzer analyzer = new StudentAnalyzer(studentList);
-        analyzer.print();
-
+        System.out.println("printStudentNameAndAge method: ");
+        analyzer.printStudentNameAndAge();
+        System.out.println("addStudentWithANameList method: ");
+        analyzer.addStudentWithANameList();
+        System.out.println("addStudentWith5OrMoreGrade method: ");
+        analyzer.printStudentWith5OrMoreGrade();
+        System.out.println("printStudentWith5OrMoreGradeNotPHP method: ");
+        analyzer.printStudentWith5OrMoreGradeNotPHP();
+        System.out.println("printAdultAndJavaStudents method: ");
+        analyzer.printAdultAndJavaStudents();
     }
 }
