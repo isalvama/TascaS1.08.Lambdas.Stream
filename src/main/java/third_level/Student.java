4aa6a1f5fb621616aaa42617e8e3cfdda4d46c01
isalvama <inesalvans@gmail.com>
2026-02-23 14:18:00 +0100

    feat: add toString() method in Student class, create StudentAnalizer class, create Student analizer object in ThirdLevelMain to test print() method of StudentAnalizer class

--- src/main/java/third_level/Student.java
+++ src/main/java/third_level/Student.java
@@ -3,13 +3,23 @@ package third_level;
 public class Student {
     private String name;
     private int age;
-    private int studyCourse;
+    private String studyCourse;
     private double grade;
 
-    public Student (String name, int age, int studyCourse, double grade){
+    public Student (String name, int age, String studyCourse, double grade){
         this.name = name;
         this.age = age;
         this.studyCourse = studyCourse;
         this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", studyCourse='" + studyCourse + '\'' +
+                ", grade=" + grade +
+                '}';
     }
 }
--- src/main/java/third_level/ThirdLevelMain.java
+++ src/main/java/third_level/ThirdLevelMain.java
@@ -1,12 +1,12 @@
 package third_level;
 
+import java.util.Arrays;
+import java.util.List;
+
 /*
 Aquest nivell introdueix l’ús de lambdas i streams aplicats a una classe pròpia, Alumne, per simular situacions reals.
 Filtraràs i transformaràs llistes d’objectes, aplicant múltiples condicions i accions. Això t’ajudarà a veure el valor
 pràctic d’aquestes eines en projectes més realistes, amb dades més estructurades.
-
-Exercici 1
-Omple una llista amb 10 alumnes
 
 Mostra per pantalla el nom i l’edat de cada alumne/a.
 Filtra la llista per tots els alumnes que el seu nom comença per ‘a’. Assigna a aquests alumnes a una altra llista i mostra per pantalla la nova llista (tot amb lambdes).
@@ -15,4 +15,22 @@ Filtra i mostra per pantalla els alumnes que tenen una nota de 5 o més, i que n
 Mostra tots els alumnes que fan JAVA i són majors d’edat.
  */
 public class ThirdLevelMain {
+    public static void main (String[] args){
+        Student student1 = new Student("Joe", 30, "PHP", 6.5);
+        Student student2 = new Student("Laura", 35, "Java", 7);
+        Student student3 = new Student("Mark", 27, "Javascript", 8);
+        Student student4 = new Student("Maria", 25, "Javascript", 6.5);
+        Student student5 = new Student("Alex", 30, "PHP", 5);
+        Student student6 = new Student("Valerie", 23, "Java", 9);
+        Student student7 = new Student("Fernando", 32, "Javascript", 6.5);
+        Student student8 = new Student("Ivan", 31, "Java", 5.5);
+        Student student9 = new Student("Paula", 26, "PHP", 6);
+        Student student10 = new Student("Natalia", 30, "Java", 9);
+
+        List<Student> studentList = Arrays.asList(student1, student2, student3, student4, student5, student6, student7, student8, student9, student10);
+
+        StudentAnalizer analizer = new StudentAnalizer(studentList);
+        analizer.print();
+
+    }
 }
